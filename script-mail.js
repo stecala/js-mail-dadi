412a93b516610283e89a2a532647daf4eac9d034
stecala <ste.cala123@gmail.com>
2022-06-17 13:31:06 +0200

    creazione repo

---
+++ script-mail.js
@@ -0,0 +1,17 @@
+let userArray = ['ginopino@gmail.com', 'cavallo@gmail.com', 'bottiglia@gmail.com', 'telefono@gmail.com', 'joystick@gmail.com', 'cuffie@gmail.com', 'mouse@gmail.com', 'laptop@gmail.com', 'cassa@gmail.com', 'occhiali@gmail.com'];
+const userMail = prompt('inserisci la tua mail per validare');
+
+let message;
+for(let index = 0, index < userArray.lenght-1, index++)
+{   
+    if(userMail===userArray[i])
+    {
+        message = console.log( 'puoi entrare' );
+    }
+    else
+    {
+        message = console.log( 'puoi entrare' );
+    }
+}
+
+console.log(message);
