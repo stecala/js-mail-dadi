023b4ae5c04d010082caa567e3aa593b951c30f0
stecala <ste.cala123@gmail.com>
2022-06-17 14:16:00 +0200

    fine mail

--- script-mail.js
+++ script-mail.js
@@ -1,17 +1,17 @@
 let userArray = ['ginopino@gmail.com', 'cavallo@gmail.com', 'bottiglia@gmail.com', 'telefono@gmail.com', 'joystick@gmail.com', 'cuffie@gmail.com', 'mouse@gmail.com', 'laptop@gmail.com', 'cassa@gmail.com', 'occhiali@gmail.com'];
 const userMail = prompt('inserisci la tua mail per validare');
 
-let message;
-for(let index = 0, index < userArray.lenght-1, index++)
+let messageValidator=false;
+for ( let i = 0 ; i<userArray.length-1 ; i++ )
 {   
-    if(userMail===userArray[i])
+    if( userMail === userArray[i] )
     {
-        message = console.log( 'puoi entrare' );
-    }
-    else
-    {
-        message = console.log( 'puoi entrare' );
+        messageValidator=true;
     }
 }
-
-console.log(message);
+let message;
+if(messageValidator==true){
+    message=console.log('mail presente')
+}else{
+    message=console.log('mail assente')
+}
